b4dbe4419ba0424568d1e451220df0006a87bb86
Aidan Cunniffe <acunniffe@gmail.com>
2022-05-19 15:29:03 -0400

    standard rulesets included

--- src/index.ts
+++ src/index.ts
@@ -2,19 +2,37 @@
 
 import { initializeCli } from "@useoptic/optic-ci/build/initialize";
 import { publicApiStandards } from "./public-api-standards";
+import {
+  NamingChangesRuleset,
+  BreakingChangesRuleset,
+} from "@useoptic/standard-rulesets";
 import { internalApiStandards } from "./internal-api-standards";
 
 (async () => {
   const cli = await initializeCli({
-    rules: (() => {
-      // make public rules the default
-      if (!Boolean(process.env.STANDARD) || process.env.STANDARD === "public")
-        return publicApiStandards;
-      if (process.env.STANDARD === "internal") return internalApiStandards;
-      return [];
-    })(),
-
-    // these will be properly set in CI
+    rules: [
+      // Configure naming rules for your API
+      new NamingChangesRuleset({
+        applies: "always", // also available: 'added' | 'addedOrChanged'
+        options: {
+          // valid formats are: 'camelCase' | 'Capital-Param-Case' | 'param-case' | 'PascalCase' | 'snake_case'
+          properties: "camelCase",
+          queryParameters: "camelCase",
+          requestHeaders: "camelCase",
+          responseHeaders: "camelCase",
+        },
+      }),
+      // Catch breaking changes
+      new BreakingChangesRuleset(),
+      // Register your custom Optic Rulesets
+      ...(() => {
+        // make public rules the default
+        if (!Boolean(process.env.STANDARD) || process.env.STANDARD === "public")
+          return publicApiStandards;
+        if (process.env.STANDARD === "internal") return internalApiStandards;
+        return [];
+      })(),
+    ],
     token: process.env.OPTIC_TOKEN || "",
     gitProvider: {
       token: process.env.GITHUB_TOKEN || "",
